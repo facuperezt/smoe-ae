a96c35f56115eb4b624cd13aee7a0e98e5425dfc
Facu <facuperezt@gmail.com>
2024-04-19 13:45:37 +0200

    modified test_models

--- analyze_models.py
+++ analyze_models.py
@@ -9,7 +9,7 @@ from utils.visualize_kernels import plot_kernels, plot_kernel_centers
 
 from PIL import Image
 
-from models.facu import VAE, SimpleMLP
+from models.facu import VAE, SimpleMLP, VAE_KernelsOutside, VAE_NegativeExperts, VAE_KernelsOutsideNegativeExperts
 from models.elvira import Vanilla as AE
 
 def set_title(ax: plt.Axes, title: str) -> None:
@@ -46,6 +46,22 @@ def load_model(who: str) -> torch.nn.Module:
         model = VAE(n_kernels=n_kernels, block_size=block_size, img_size=img_size, load_tf_model=False, device=device)
         path = "C:\\Users\\fq\\Facu\\clean-smoe\\models\\facu\\checkpoints\\vae\\final.pth"
         model.load_state_dict(torch.load(path))
+    elif who == "vae_kernels_inside":
+        model = VAE(n_kernels=n_kernels, block_size=block_size, img_size=img_size, load_tf_model=False, device=device)
+        path = "C:\\Users\\fq\\Facu\\clean-smoe\\models\\facu\\checkpoints\\vae_kernels_inside\\final.pth"
+        model.load_state_dict(torch.load(path))
+    elif who == "vae_kernels_outside":
+        model = VAE_KernelsOutside(n_kernels=n_kernels, block_size=block_size, img_size=img_size, load_tf_model=False, device=device)
+        path = "C:\\Users\\fq\\Facu\\clean-smoe\\models\\facu\\checkpoints\\vae_kernels_outside\\final.pth"
+        model.load_state_dict(torch.load(path))
+    elif who == "vae_negative_experts":
+        model = VAE_NegativeExperts(n_kernels=n_kernels, block_size=block_size, img_size=img_size, load_tf_model=False, device=device)
+        path = "C:\\Users\\fq\\Facu\\clean-smoe\\models\\facu\\checkpoints\\vae_negative_experts\\final.pth"
+        model.load_state_dict(torch.load(path))
+    elif who == "vae_kernels_outside_negative_experts":
+        model = VAE_KernelsOutsideNegativeExperts(n_kernels=n_kernels, block_size=block_size, img_size=img_size, load_tf_model=False, device=device)
+        path = "C:\\Users\\fq\\Facu\\clean-smoe\\models\\facu\\checkpoints\\vae_kernels_outside_negative_experts\\final.pth"
+        model.load_state_dict(torch.load(path))
     else:
         raise ValueError(f"Who is {who}?")
     return model
@@ -61,58 +77,50 @@ def visualize_output(model, blocked_output: torch.Tensor, cmap: str = 'gray', vm
     else:
         ax.imshow(img, cmap=cmap, vmin=vmin, vmax=vmax)
 
-for model, model_name in zip(
-            [
-                # load_model("elvira"),
-                load_model("vae"),
-                # load_model("mlp")
-            ],
-            [
-                # "elvira_small",
-                "vae",
-                # "mlp_big",
-            ],
-    ):
-    os.makedirs(f"models/facu/kernel_pics/{model_name}/", exist_ok=True)
-    model.eval()
-    img = Image.open("data/professional_photos/train/ali-inay-2273.png").convert("L")
-    img_arr = (torch.tensor(np.array(img), dtype=torch.float32, device=device, requires_grad=False)/255)
-    img_arr = img_arr[None, :img_size, :img_size]
-    blocks = model.img2block(img_arr)
-    emb = model.encoder(blocks)
-    if model_name == "elvira_small":
-        emb = model.clipper(emb)
-    elif model_name == "vae":
-        emb = emb[0]
-    out = model.decoder(emb).cpu()
-    emb = emb.cpu()
-    blocks = blocks.cpu()
-    colors = ["tab:blue", "tab:orange", "tab:green", "tab:purple"]
-    for i, (emb_i, block_i, out_i) in enumerate(zip(emb, blocks.squeeze(), out)):
-        plt.close()
-        fig, axs = plt.subplots(2, 2, figsize=(10, 10))
-        fig.suptitle(f"{model_name}")
-        ax = axs[1]
-        # add padding around out_i
-        padding = [5, 5, 5, 5]
-        out_i = torch.nn.functional.pad(out_i, padding, value=1)
-        visualize_output(model, out_i, ax=ax[0])
-        plot_kernel_centers(emb_i, ax=ax[0], block_size=block_size, padding=padding, colors=colors)
-        plot_kernels(emb_i, ax=ax[0], block_size=block_size, padding=padding, colors=colors)
-        ax[0].axis("off")
-        
-        block_i = torch.nn.functional.pad(block_i, padding, value=1)
-        visualize_output(model, block_i, ax=ax[1])
-        plot_kernel_centers(emb_i, ax=ax[1], block_size=block_size, padding=padding, colors=colors)
-        plot_kernels(emb_i, ax=ax[1], block_size=block_size, padding=padding, colors=colors)
-        ax[1].axis("off")
+if __name__ == "__main__":
+    for model_name in ["vae_kernels_inside", "vae_kernels_outside", "vae_negative_experts", "vae_kernels_outside_negative_experts"]:
+        model = load_model(model_name)
+        os.makedirs(f"models/facu/kernel_pics/{model_name}/", exist_ok=True)
+        model.eval()
+        img = Image.open("data/professional_photos/train/ali-inay-2273.png").convert("L")
+        img_arr = (torch.tensor(np.array(img), dtype=torch.float32, device=device, requires_grad=False)/255)
+        img_arr = img_arr[None, :img_size, :img_size]
+        blocks = model.img2block(img_arr)
+        emb = model.encoder(blocks)
+        if model_name == "elvira_small":
+            emb = model.clipper(emb)
+        elif "vae" in model_name:
+            emb = emb[0]
+        out = model.decoder(emb).cpu()
+        emb = emb.cpu()
+        blocks = blocks.cpu()
+        colors = ["tab:blue", "tab:orange", "tab:green", "tab:purple"]
+        for i, (emb_i, block_i, out_i) in enumerate(zip(emb, blocks.squeeze(), out)):
+            plt.close()
+            fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+            fig.suptitle(f"{model_name}")
+            ax = axs[1]
+            # add padding around out_i
+            padding = [5, 5, 5, 5]
+            out_i = torch.nn.functional.pad(out_i, padding, value=1)
+            visualize_output(model, out_i, ax=ax[0])
+            plot_kernel_centers(emb_i, ax=ax[0], block_size=block_size, padding=padding, colors=colors)
+            plot_kernels(emb_i, ax=ax[0], block_size=block_size, padding=padding, colors=colors)
+            ax[0].axis("off")
+            
+            block_i = torch.nn.functional.pad(block_i, padding, value=1)
+            visualize_output(model, block_i, ax=ax[1])
+            plot_kernel_centers(emb_i, ax=ax[1], block_size=block_size, padding=padding, colors=colors)
+            plot_kernels(emb_i, ax=ax[1], block_size=block_size, padding=padding, colors=colors)
+            ax[1].axis("off")
 
-        ax = axs[0]
-        # out_i = torch.nn.functional.pad(out_i, padding, value=1)
-        visualize_output(model, out_i, ax=ax[0])
-        ax[0].axis("off")
+            ax = axs[0]
+            # out_i = torch.nn.functional.pad(out_i, padding, value=1)
+            visualize_output(model, out_i, ax=ax[0])
+            ax[0].axis("off")
 
-        # block_i = torch.nn.functional.pad(block_i, padding, value=1)
-        visualize_output(model, block_i, ax=ax[1])
-        ax[1].axis("off")
-        plt.savefig(f"models/facu/kernel_pics/{model_name}/{i}_block.png")
+            # block_i = torch.nn.functional.pad(block_i, padding, value=1)
+            visualize_output(model, block_i, ax=ax[1])
+            ax[1].axis("off")
+            plt.show()
+            # plt.savefig(f"models/facu/kernel_pics/{model_name}/{i}_block.png")
--- test_model.py
+++ test_model.py
@@ -4,7 +4,9 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 from models.elvira import Vanilla
-from models.facu import VAE
+from models.facu import VAE, VAE_KernelsOutside, VAE_NegativeExperts, VAE_KernelsOutsideNegativeExperts, SimpleMLP
+
+from analyze_models import load_model
 
 def test_elvira_vanilla():
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -20,7 +22,7 @@ def test_elvira_vanilla():
 
 def test_vanilla_VAE():
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = VAE(n_kernels=4, block_size=16, device=device)
+    model = load_model("vae")
     model.eval()
     img = Image.open("data/professional_photos/train/ali-inay-2273.png").convert("L")
     img = (torch.tensor(np.array(img), dtype=torch.float32, device=device, requires_grad=False)/255)
@@ -30,7 +32,46 @@ def test_vanilla_VAE():
     plt.show()
     print("Done")
 
+def test_VAE_KernelsOutside():
+    n_kernels, block_size, img_size = 4, 16, 128
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = load_model("vae_kernels_outside")
+    model.eval()
+    img = Image.open("data/professional_photos/train/ali-inay-2273.png").convert("L")
+    img = (torch.tensor(np.array(img), dtype=torch.float32, device=device, requires_grad=False)/255)
+    x = img[None, :img_size, :img_size]
+    y = model(x)[0]
+    plt.imshow(y.detach().cpu().numpy().squeeze(), cmap='gray', vmin=0, vmax=1)
+    plt.show()
+    print("Done")
+
+def test_VAE_NegativeExperts():
+    n_kernels, block_size, img_size = 4, 16, 128
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = VAE_NegativeExperts(n_kernels=n_kernels, block_size=block_size, img_size=img_size, device=device)
+    model.eval()
+    img = Image.open("data/professional_photos/train/ali-inay-2273.png").convert("L")
+    img = (torch.tensor(np.array(img), dtype=torch.float32, device=device, requires_grad=False)/255)
+    x = img[None, :img_size, :img_size]
+    y = model(x)
+    plt.imshow(y.detach().cpu().numpy().squeeze(), cmap='gray', vmin=0, vmax=1)
+    plt.show()
+    print("Done")
+
+def test_VAE_KernelsOutsideNegativeExperts():
+    n_kernels, block_size, img_size = 4, 16, 128
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = VAE_KernelsOutsideNegativeExperts(n_kernels=n_kernels, block_size=block_size, img_size=img_size, device=device)
+    model.eval()
+    img = Image.open("data/professional_photos/train/ali-inay-2273.png").convert("L")
+    img = (torch.tensor(np.array(img), dtype=torch.float32, device=device, requires_grad=False)/255)
+    x = img[None, :img_size, :img_size]
+    y = model(x)
+    plt.imshow(y.detach().cpu().numpy().squeeze(), cmap='gray', vmin=0, vmax=1)
+    plt.show()
+    print("Done")
+
 if __name__ == "__main__":
     # test_elvira_vanilla()
-    test_vanilla_VAE()
+    test_VAE_NegativeExperts()
     plt.show()
