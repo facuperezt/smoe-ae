b3effb48b9b2eaf06ce55135213e9c2846aafc3c
Facu <facuperezt@gmail.com>
2024-05-13 19:52:49 +0200

    train with data simulation

--- train_model_on_random_blocks.py
+++ train_model_on_random_blocks.py
@@ -285,8 +285,8 @@ if __name__ == "__main__":
 
     hidden_dims = [2, 4, 8, 16, 32, 64, 128, 256]
 
-    nr_epochs = 2000
-    nr_batches = 50
+    nr_epochs = 1500
+    nr_batches = 25
     batch_size = 1_000_000
     load_final = False
     input_is_img = False
